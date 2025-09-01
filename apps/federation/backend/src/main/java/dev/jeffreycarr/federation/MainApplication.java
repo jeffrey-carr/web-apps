eb6279a62fbb9c9036125d38c0de0e5a5112266f
Jeffrey Carr <jeffrey.carr98@gmail.com>
2025-09-01 10:04:10 -0400

    mongo fixing

--- apps/federation/backend/src/main/java/dev/jeffreycarr/federation/MainApplication.java
+++ apps/federation/backend/src/main/java/dev/jeffreycarr/federation/MainApplication.java
@@ -2,10 +2,13 @@
 package dev.jeffreycarr.federation;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 @ComponentScan(basePackages = {"dev.jeffreycarr.federation", "dev.jeffreycarr.javacommon"})
 public class MainApplication {
 
