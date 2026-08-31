dc207777a10a70f068c523afca573f418dee1b39
Jeff Carr <jeffrey.carr98@gmail.com>
2026-08-30 21:42:13 -0400

    add logging

--- apps/web_games/backend/domains/user/handler.go
+++ apps/web_games/backend/domains/user/handler.go
@@ -2,35 +2,40 @@ package user
 
 import (
 	"context"
-	"fmt"
 	"go-common/jcontext"
 	"go-common/jhttp"
 	"go-common/jhttp/errors"
+	"go-common/jlogging"
 	"jeffs-web-games/user"
 )
 
 // Handler represents the user handler
 type Handler struct {
 	controller user.Controller
+	logger     jlogging.Logger
 }
 
 // NewHandler creates a new user handler
-func NewHandler(controller user.Controller) Handler {
+func NewHandler(controller user.Controller, logger jlogging.Logger) Handler {
 	return Handler{
 		controller: controller,
+		logger:     logger,
 	}
 }
 
 // GetMe gets the profile of the logged in user
 func (h Handler) GetMe(ctx context.Context, _ jhttp.RequestNoData) (*user.UserProfile, *errors.JHTTPError) {
+	logger := h.logger.WithField("action", "getMe")
+
 	user, userPresent := jcontext.GetUser(ctx)
 	if !userPresent {
-		return nil, errors.NewInternalServerError(errors.NewInternalServerError(fmt.Errorf("this wasnt supposed to get this far")))
-		// return nil, errors.NewUnauthorizedError()
+		return nil, errors.NewUnauthorizedError()
 	}
+	logger.WithField(jlogging.UserUUIDLogLabel, user.UUID)
 
 	profile, err := h.controller.GetMe(ctx, user)
 	if err != nil {
+		logger.WithError(err).Error("failed to get user profile")
 		return nil, errors.NewInternalServerError(err)
 	}
 
--- apps/web_games/backend/domains/word_chain/handler.go
+++ apps/web_games/backend/domains/word_chain/handler.go
@@ -2,10 +2,10 @@ package word_chain
 
 import (
 	"context"
-	"fmt"
 	"go-common/jcontext"
 	"go-common/jhttp"
 	"go-common/jhttp/errors"
+	"go-common/jlogging"
 	"jeffs-web-games/stats"
 	"jeffs-web-games/word_chain"
 	"strings"
@@ -14,36 +14,50 @@ import (
 type Handler struct {
 	controller word_chain.Controller
 	stats      stats.Controller
+	logger     jlogging.Logger
 }
 
-func NewHandler(controller word_chain.Controller, statsController stats.Controller) Handler {
+func NewHandler(
+	controller word_chain.Controller,
+	statsController stats.Controller,
+	logger jlogging.Logger,
+) Handler {
 	return Handler{
 		controller: controller,
 		stats:      statsController,
+		logger:     logger,
 	}
 }
 
 // NewGame creates a new word chain game
 func (h Handler) NewGame(ctx context.Context, _ jhttp.RequestData[struct{}]) (*word_chain.GamePackage, *errors.JHTTPError) {
+	logger := h.logger.WithField("action", "newGame")
+
 	game, err := h.controller.NewGame(ctx)
 	if err != nil {
+		logger.WithError(err).Error("failed to create new game")
 		return nil, errors.NewInternalServerError(err)
 	}
+	logger = logger.WithField("game", game)
 
 	user, userPresent := jcontext.GetUser(ctx)
 	if userPresent {
+		logger = logger.WithField(jlogging.UserUUIDLogLabel, user.UUID)
 		_, err = h.stats.UpdateGameStats(ctx, user, stats.GameNameWordChain, stats.TypePlayed)
 		// Don't blow up the whole request because we failed to record the stats... we should just
 		// log the error
 		if err != nil {
-			// TODO: log
+			logger.WithError(err).Error("failed to update user's game stats")
 		}
 	}
 
+	logger.Info("created new game")
 	return &game, nil
 }
 
 func (h Handler) ValidateGuess(ctx context.Context, r jhttp.RequestData[word_chain.ValidateGuessRequest]) (*word_chain.ValidateGuessResponse, *errors.JHTTPError) {
+	logger := h.logger.WithField("action", "validateGuess")
+
 	if r.Body == nil {
 		return nil, errors.NewBadRequestError("Bad request")
 	}
@@ -53,20 +67,23 @@ func (h Handler) ValidateGuess(ctx context.Context, r jhttp.RequestData[word_cha
 	if strings.TrimSpace(r.Body.EncryptedState) == "" {
 		return nil, errors.NewBadRequestError("Game state is required")
 	}
+	logger.WithField("guess", r.Body.Guess)
 
-	fmt.Printf("Validating word chain guess \"%s\"...\n", r.Body.Guess)
 	response, err := h.controller.ValidateGuess(ctx, *r.Body)
 	if err != nil {
+		logger.WithError(err).Error("failed to validate guess")
 		return nil, errors.NewInternalServerError(err)
 	}
 
 	user, userPresent := jcontext.GetUser(ctx)
 	if userPresent && response.Victory {
+		logger = logger.WithField(jlogging.UserUUIDLogLabel, user.UUID)
+		logger.Info("user made guess")
 		_, err = h.stats.UpdateGameStats(ctx, user, stats.GameNameWordChain, stats.TypeCompleted)
 		// Don't blow up the whole request because we failed to record the stats... we should just
 		// log the error
 		if err != nil {
-			// TODO: log
+			logger.WithError(err).Error("failed to update user stats")
 		}
 	}
 
